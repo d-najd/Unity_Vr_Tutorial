55d29dc2f451bcaab76432cb1eeabb451ac8325a
najd <dimitar.hristijan.najdovski@gmail.com>
2023-05-30 16:35:50 +0200

    Part 6

---
+++ Assets/Scripts/XRGrabInteractableTwoAttach.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabInteractableTwoAttach : XRGrabInteractable
+{
+    [SerializeField] private Transform leftAttachTransform;
+    [SerializeField] private Transform rightAttachTransform;
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.CompareTag("Left Hand"))
+        {
+            attachTransform = leftAttachTransform;
+        }
+        else if (args.interactorObject.transform.CompareTag("Right Hand"))
+        {
+            attachTransform = rightAttachTransform;
+        }
+        else
+        {
+            Debug.LogError($"Interactable Missing Attach Point {args.interactableObject.transform.tag} {args.interactableObject.transform.name}");
+        }
+
+        base.OnSelectEntered(args);
+    }
+}
